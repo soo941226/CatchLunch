f64e244a89259172a313d7c446c2eeceb6432c45
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-02-24 18:33:29 +0900

    feat: added RestaurantInformation and related things

---
+++ CatchLunch/CatchLunch/Model/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  CatchLunch
+//
+//  Created by kjs on 2022/02/23.
+//
+
+protocol Restaurant {
+    var isBookmarked: Bool { get }
+    var cityName: String { get }
+    var restaurantName: String { get }
+    var contact: String? { get }
+    var refinedZipCode: String? { get }
+    var roadNameAddress: String? { get }
+    var locationNameAddress: String? { get }
+    var mainFoodName: String? { get }
+}
---
+++ CatchLunch/CatchLunch/Model/RestaurantInformation.swift
@@ -0,0 +1,21 @@
+//
+//  RestaurantInformation.swift
+//  CatchLunch
+//
+//  Created by kjs on 2022/02/24.
+//
+
+import Foundation
+
+struct RestaurantInformation: Restaurant, Coordinate, Bookmarkable {
+    private(set) var isBookmarked: Bool
+    private(set) var cityName: String
+    private(set) var restaurantName: String
+    private(set) var contact: String?
+    private(set) var refinedZipCode: String?
+    private(set) var roadNameAddress: String?
+    private(set) var locationNameAddress: String?
+    private(set) var mainFoodName: String?
+    private(set) var latitude: Double
+    private(set) var longitude: Double
+}
