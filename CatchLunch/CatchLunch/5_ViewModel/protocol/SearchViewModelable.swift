719fc56c0497cb606b75e0926ae4f5b9183b959f
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-04 15:50:02 +0900

    refactor: changed SearchViewModelable
    * added JustSearchViewModelable
    * added NameSearchViewModelable
    * renamed RestaurantViewModel from GyeonggiRestaurantViewModel

--- CatchLunch/CatchLunch/5_ViewModel/protocol/SearchViewModelable.swift
+++ CatchLunch/CatchLunch/5_ViewModel/protocol/SearchViewModelable.swift
@@ -5,12 +5,17 @@
 //  Created by kjs on 2022/02/27.
 //
 
-protocol SearchViewModelable {
-    associatedtype Service: SearchService
-    typealias Response = Service.Response
+// TODO: - handling error in each viewModel
 
+protocol SearchViewModelable {
+    associatedtype Service
     init(service: Service)
-    var service: Service { get }
-    
+}
+
+protocol JustSearchViewModelable: SearchViewModelable {
     func fetch(completionHandler: @escaping (Bool) -> Void)
+}
+
+protocol NameSearchViewModelable: SearchViewModelable {
+    func fetch(about name: String ,completionHandler: @escaping (Bool) -> Void)
 }
