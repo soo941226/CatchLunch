f59acaa6b7a701e0211176750b8b5eb2b5778ddc
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-08 15:17:46 +0900

    feat: added empty DetailViewController

---
+++ CatchLunch/CatchLunch/7_Controller/DetailView/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  CatchLunch
+//
+//  Created by kjs on 2022/03/08.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        print(#function)
+    }
+}
