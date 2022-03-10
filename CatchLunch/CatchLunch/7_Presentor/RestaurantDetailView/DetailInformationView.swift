ecdc053c7eb163043ab12bb3730d3f8e84b14b96
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-10 14:38:50 +0900

    refactor: moved views to view from controller

--- CatchLunch/CatchLunch/7_Presentor/RestaurantDetailView/DetailInformationView.swift
+++ CatchLunch/CatchLunch/7_Presentor/RestaurantDetailView/DetailInformationView.swift
@@ -8,5 +8,51 @@
 import UIKit
 
 final class DetailInformationView: UIView {
+    private let stackView = UIStackView()
+    private let imageView = UIImageView()
+    private let cityNameLabel = UILabel()
+    private let restaurantNameLabel = UILabel()
+    private let phoneNumberLabel = UILabel()
+    private let roadAddressLabel = UILabel()
+    private let locationAddressLabel = UILabel()
+    private let mainFoodsLabel = UILabel()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        stackView
+            .configure(
+                axis: .vertical, distribution: .fillProportionally,
+                alignment: .leading, spacing: 8.0)
+            .addArrangedSubviews(
+                imageView, cityNameLabel, restaurantNameLabel, phoneNumberLabel,
+                roadAddressLabel, locationAddressLabel, mainFoodsLabel
+            )
+
+        let safeArea = safeAreaLayoutGuide
+
+        addSubview(stackView)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    func configure(with summary: RestaurantSummary) {
+        imageView.image = summary.image
+        cityNameLabel.text = summary.information.cityName
+        restaurantNameLabel.text = summary.information.restaurantName
+        phoneNumberLabel.text = summary.information.phoneNumber
+        roadAddressLabel.text = summary.information.roadNameAddress
+        locationAddressLabel.text = summary.information.locationNameAddress
+        mainFoodsLabel.text = summary.information.descriptionOfMainFoodNames
+    }
 }
--- CatchLunch/CatchLunch/7_Presentor/RestaurantDetailView/DetailViewController.swift
+++ CatchLunch/CatchLunch/7_Presentor/RestaurantDetailView/DetailViewController.swift
@@ -9,13 +9,7 @@ import UIKit
 
 final class DetailViewController: UIViewController {
     private let summary: RestaurantSummary
-    private let imageView = UIImageView()
-    private let cityNameLabel = UILabel()
-    private let restaurantNameLabel = UILabel()
-    private let phoneNumberLabel = UILabel()
-    private let roadAddressLabel = UILabel()
-    private let locationAddressLabel = UILabel()
-    private let mainFoodsLabel = UILabel()
+    private let detailView = DetailInformationView()
 
     init(with model: RestaurantSummary) {
         self.summary = model
@@ -26,37 +20,13 @@ final class DetailViewController: UIViewController {
         fatalError(.meesageAboutInterfaceBuilder)
     }
 
-    let stackView = UIStackView()
+    override func loadView() {
+        view = detailView
+        view.backgroundColor = .systemBackground
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-
-        stackView
-            .configure(
-                axis: .vertical, distribution: .fillProportionally,
-                alignment: .leading, spacing: 8.0)
-            .addArrangedSubviews(
-                imageView, cityNameLabel, restaurantNameLabel, phoneNumberLabel,
-                roadAddressLabel, locationAddressLabel, mainFoodsLabel
-            )
-
-        let safeArea = view.safeAreaLayoutGuide
-
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
-        ])
-
-        imageView.image = summary.image
-        cityNameLabel.text = summary.information.cityName
-        restaurantNameLabel.text = summary.information.restaurantName
-        phoneNumberLabel.text = summary.information.phoneNumber
-        roadAddressLabel.text = summary.information.roadNameAddress
-        locationAddressLabel.text = summary.information.locationNameAddress
-        mainFoodsLabel.text = summary.information.descriptionOfMainFoodNames
+        detailView.configure(with: summary)
     }
 }
