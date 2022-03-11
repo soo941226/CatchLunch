a755b41f122c369e6005754804ad05ad587ac0ff
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-11 21:09:53 +0900

    feat: extended String with prepend

--- CatchLunch/CatchLunch/Extension/Foundation/String+.swift
+++ CatchLunch/CatchLunch/Extension/Foundation/String+.swift
@@ -7,4 +7,12 @@
 
 extension String {
     static let meesageAboutInterfaceBuilder = "Do not create any cell with InterfaceBuilder"
+
+    func prepend<SomeString: StringProtocol>(_ string: SomeString?) -> String {
+        guard let string = string else {
+            return self
+        }
+
+        return string + self
+    }
 }
