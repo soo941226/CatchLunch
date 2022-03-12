8fb66f789fc140c039012947bef7aca218ad1df8
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-12 18:16:17 +0900

    test: GyeonggiRestaurantsSearcher

--- CatchLunch/CatchLunchTests/TestGyeonggiRestaurantsSearcher.swift
+++ CatchLunch/CatchLunchTests/TestGyeonggiRestaurantsSearcher.swift
@@ -17,8 +17,10 @@ final class TestGyeonggiRestaurantsSearcher: XCTestCase {
     override func tearDown() {
         restaurantSearcherUnderTest = nil
     }
+    
     func test_request를_잘못던지면_clientError를_던진다() {
         // given
+        setUpHandler(data: Data(), code: 400)
         let dispatch = XCTestExpectation(description: "expect error")
         let expectation = NetworkError.clientError(code: 400).errorDescription
         var resultToExpect = ""
@@ -48,6 +50,7 @@ final class TestGyeonggiRestaurantsSearcher: XCTestCase {
 
     func test_pageIndex를0부터요청하면_데이터가없다는에러가뜬다() {
         // given
+        setUpHandler(data: Data(), code: 200)
         let dispatch = XCTestExpectation(description: "expect error")
         let expectation = NetworkError.dataIsNotExist.errorDescription
         var resultToExpect = ""
@@ -77,6 +80,7 @@ final class TestGyeonggiRestaurantsSearcher: XCTestCase {
 
     func test_itemAmount를0이하로요청하면_데이터가_오지_않는다() {
         // given
+        setUpHandler(data: DummyGyeonggiAPIResult().emptyObject, code: 200)
         let dispatch = XCTestExpectation(description: "expect error")
         let expectation = Int.zero
         var resultToExpect = Int.zero
@@ -105,6 +109,7 @@ final class TestGyeonggiRestaurantsSearcher: XCTestCase {
 
     func test_알수없는상황에_잘못된응답이와도_fatalError는_나지않는다() {
         // given
+        setUpHandler(data: Data(), code: 1234, error: DummyError())
         let dispatch = XCTestExpectation(description: "expect error")
 
         // when
@@ -122,8 +127,36 @@ final class TestGyeonggiRestaurantsSearcher: XCTestCase {
         wait(for: [dispatch], timeout: 5.0)
     }
 
+
+    func test_서버에서_잘못된_데이터를_내려주면_디코딩에러가_난다() {
+        // given
+        setUpHandler(data: DummyGyeonggiAPIResult().objectWithWrongFormmat, code: 200)
+        let dispatch = XCTestExpectation(description: "expect error")
+        var resultToExpeect: DecodingError?
+        // when
+        restaurantSearcherUnderTest.fetch(
+            itemPageIndex: .max, requestItemAmount: .max
+        ) { result in
+            switch result {
+            case .success:
+                XCTFail("error")
+            case .failure(let error):
+                guard let error = error as? DecodingError else {
+                    XCTFail(error.localizedDescription)
+                    return
+                }
+                resultToExpeect = error
+            }
+            dispatch.fulfill()
+        }
+        wait(for: [dispatch], timeout: 5.0)
+
+        XCTAssertNotNil(resultToExpeect)
+    }
+
     func test_상식적인pageIndex와amount를던지면_응답이잘온다() {
         // given
+        setUpHandler(data: DummyGyeonggiAPIResult().objectWithCount10, code: 200)
         let dispatch = XCTestExpectation(description: "expect error")
         let expectation = 10
         var resultToExpect = Int.zero
