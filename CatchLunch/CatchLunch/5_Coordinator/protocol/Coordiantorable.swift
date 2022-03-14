679ddb656f6ba6e01732d65c2ad9973468329bc3
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-03-14 23:08:05 +0900

    feat: added protocol ParentCoodiantor
    * it has RestaurantSummary now. but I will change it (maybe...)

--- CatchLunch/CatchLunch/5_Coordinator/protocol/Coordiantorable.swift
+++ CatchLunch/CatchLunch/5_Coordinator/protocol/Coordiantorable.swift
@@ -6,6 +6,10 @@
 //
 import UIKit
 
+protocol ParentCoordinator: AnyObject {
+    var model: RestaurantSummary? { get }
+}
+
 protocol Coordiantorable: AnyObject {
     init(on navigationController: UINavigationController)
     var childCoodinator: [Coordiantorable] { get }
