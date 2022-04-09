c2bde6f47c105224cfc89ff4da7082a901893841
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-04-09 21:37:08 +0900

    feat: added requesting and rendering feature to MapRoutingViewController

--- CatchLunch/CatchLunch/6_Presentor/MapRoutingView/MapRoutingViewController.swift
+++ CatchLunch/CatchLunch/6_Presentor/MapRoutingView/MapRoutingViewController.swift
@@ -24,6 +24,33 @@ final class MapRoutingViewController: UIViewController {
 
     override func loadView() {
         view = mapView
-        mapView.setCameraZoomRange(.init(maxCenterCoordinateDistance: 1000), animated: false)
+        mapView.delegate = self
+        mapView.setCameraZoomRange(.init(maxCenterCoordinateDistance: 10000), animated: false)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.route() { routes in
+            guard let route = routes?.first,
+                  let startPlace = self.viewModel.startPlace else {
+                return
+            }
+            self.mapView.setCenter(startPlace.placemark.coordinate, animated: false)
+            self.mapView.addOverlay(route.polyline)
+            route.steps.forEach { eachResult in
+                print(eachResult.instructions)
+            }
+        }
+    }
+}
+
+extension MapRoutingViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        guard let overlay = overlay as? MKPolyline else { return .init() }
+
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.lineWidth = 1.0
+        renderer.strokeColor = .red
+        return renderer
     }
 }
