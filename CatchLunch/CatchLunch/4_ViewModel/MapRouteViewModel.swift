e34c31d97b372cf02dfb81a41d8a5918bd88abed
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-04-09 21:34:10 +0900

    refactor: MapRouteViewModel
    * to have and handle LocationService and MapRouter

--- CatchLunch/CatchLunch/4_ViewModel/MapRouteViewModel.swift
+++ CatchLunch/CatchLunch/4_ViewModel/MapRouteViewModel.swift
@@ -8,22 +8,47 @@
 import MapKit
 
 final class MapRouteViewModel {
-    private let service: MapRoutingService
+    private let locationService = LocationService()
+    private let mapRoutingService = MapRouter()
+    private(set) var startPlace: MKMapItem?
+    private(set) var endPlace: MKMapItem
+    private(set) var error: Error?
 
-    init(service: MapRoutingService = MapRoutingService()) {
-        self.service = service
+    init(endPoint: CLLocationCoordinate2D) {
+        endPlace = .init(placemark: .init(coordinate: endPoint))
     }
 
     func route(
-        to endPoint: CLLocationCoordinate2D,
-        then completionHandler: @escaping (MKRoute) -> Void
+        then completionHandler: @escaping ([MKRoute]?) -> Void
     ) {
-        service.requestRouting(toY: endPoint.latitude, andX: endPoint.longitude) { result in
-            switch result {
-            case .success(let routes):
-                print(routes)
+        locationService.setUp { [weak self] resultOfLocation in
+            guard let self = self else { return }
+            switch resultOfLocation {
+            case .success(let startPlace):
+                self.startPlace = startPlace
+                self.requestRoute(from: startPlace, to: self.endPlace, with: completionHandler)
             case .failure(let error):
-                print(error)
+                self.error = error
+                completionHandler(nil)
+            }
+        }
+    }
+
+    private func requestRoute(
+        from startPlace: MKMapItem,
+        to endPlace: MKMapItem,
+        with completionHandler: @escaping ([MKRoute]?) -> Void
+    ) {
+        mapRoutingService.requestRouting(
+            from: startPlace,
+            to: endPlace
+        ) { resultOfMapRouting in
+            switch resultOfMapRouting {
+            case .success(let routes):
+                completionHandler(routes)
+            case .failure(let error):
+                self.error = error
+                completionHandler(nil)
             }
         }
     }
