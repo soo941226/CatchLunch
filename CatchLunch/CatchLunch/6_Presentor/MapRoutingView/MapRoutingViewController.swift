a3a0dc67f6746ed3101acf3d1f5a6ee204bbcc6c
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-04-10 16:09:28 +0900

    feat: added MapRoutingViewController

--- CatchLunch/CatchLunch/6_Presentor/MapRoutingView/MapRoutingViewController.swift
+++ CatchLunch/CatchLunch/6_Presentor/MapRoutingView/MapRoutingViewController.swift
@@ -9,8 +9,26 @@ import MapKit
 
 final class MapRoutingViewController: UIViewController {
     private let viewModel: MapRouteViewModel
-    private let mapView = MKMapView()
     private weak var coordiantor: Coordinatorable?
+
+    private let tableView = UITableView()
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.isRotateEnabled = false
+        mapView.isPitchEnabled = false
+        mapView.showsTraffic = false
+        return mapView
+    }()
+    private let distanceLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.backgroundColor = .systemBackground
+        label.directionalLayoutMargins = .init(dx: .headInset, dy: .headInset)
+        label.layer.cornerRadius = .cornerRadius
+        return label
+    }()
+
+    private var guideDescriptions = [String]()
 
     init(viewModel: MapRouteViewModel, under coordiantor: Coordinatorable? = nil) {
         self.viewModel = viewModel
@@ -22,25 +40,75 @@ final class MapRoutingViewController: UIViewController {
         fatalError(.meesageAboutInterfaceBuilder)
     }
 
-    override func loadView() {
-        view = mapView
-        mapView.delegate = self
-        mapView.setCameraZoomRange(.init(maxCenterCoordinateDistance: 10000), animated: false)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMapView()
+        configureTableView()
+        configureLabel()
+        configureLayout()
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        viewModel.route() { routes in
-            guard let route = routes?.first,
+        viewModel.route() { [weak self] routes in
+            guard let self = self,
+                  let route = routes?.first,
                   let startPlace = self.viewModel.startPlace else {
                 return
             }
+
             self.mapView.setCenter(startPlace.placemark.coordinate, animated: false)
+            self.mapView.setCameraZoomRange(.init(maxCenterCoordinateDistance: route.distance), animated: false)
             self.mapView.addOverlay(route.polyline)
-            route.steps.forEach { eachResult in
-                print(eachResult.instructions)
+
+            let distance = route.distance / 1000
+            self.distanceLabel.text = distance.rounded().description + "km"
+
+            let array: [String] = route.steps.compactMap { step in
+                if step.instructions.isEmpty { return nil }
+
+                let distanceDescription = step.distance.rounded().description
+                return distanceDescription + "m, " + step.instructions
             }
+            self.guideDescriptions = array
+            self.tableView.reloadData()
         }
+    }
+
+    private func configureMapView() {
+        mapView.insert(into: view)
+        mapView.delegate = self
+    }
+
+    private func configureLabel() {
+        distanceLabel.insert(into: view)
+    }
+
+    private func configureTableView() {
+        tableView.insert(into: view)
+        tableView.dataSource = self
+        tableView.separatorColor = .label
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CELL")
+    }
+
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        distanceLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: tableView.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.heightAnchor.constraint(equalTo: mapView.heightAnchor),
+            distanceLabel.topAnchor.constraint(equalTo: mapView.topAnchor, constant: .headInset),
+            distanceLabel.trailingAnchor.constraint(equalTo: mapView.trailingAnchor, constant: .tailInset)
+        ])
     }
 }
 
@@ -52,5 +120,18 @@ extension MapRoutingViewController: MKMapViewDelegate {
         renderer.lineWidth = 1.0
         renderer.strokeColor = .red
         return renderer
+    }
+}
+
+extension MapRoutingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guideDescriptions.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        cell.textLabel?.text = guideDescriptions[indexPath.row]
+        cell.textLabel?.numberOfLines = .zero
+        return cell
     }
 }
