43333f3cf53f4c9dc9726c08a31c7e333c257e21
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-04-11 16:00:18 +0900

    feat: extended UIImage to have static image

--- CatchLunch/CatchLunch/Extension/UIKit/UIImage+.swift
+++ CatchLunch/CatchLunch/Extension/UIKit/UIImage+.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 extension UIImage {
+    static let forkKnifeCircle = UIImage(systemName: "fork.knife.circle")!
+    static let starFill = UIImage(systemName: "star.fill")!
+    static let star = UIImage(systemName: "star")!
+    static let yum = UIImage(named: "yum")!
+    static let cook = UIImage(named: "cook")!
+    static let gearshapeFill = UIImage(systemName: "gearshape.fill")!
+
     func filled(with color: UIColor) -> UIImage {
         return self.withTintColor(color, renderingMode: .alwaysOriginal)
     }
